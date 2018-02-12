247b3ee854575de805a45b7292ea27c527633d19
I am <34835658+LogicZMaksimka@users.noreply.github.com>
2018-02-12 16:56:37 +0300

    Final Version of reading and writing in files
    Aaxaxaxaxaxaxaxaxaxaxaxaxaxaxaaaxxaxaxaxaxaxa

--- MainActivity.java
+++ MainActivity.java
@@ -1,104 +1,75 @@
 package com.example.student.coords;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+         import android.support.v7.app.AppCompatActivity;
+         import android.os.Bundle;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.StringWriter;
+         import java.io.BufferedReader;
+         import java.io.BufferedWriter;
+         import java.io.File;
+         import java.io.FileInputStream;
+         import java.io.FileNotFoundException;
+         import java.io.FileOutputStream;
+         import java.io.FileReader;
+         import java.io.FileWriter;
+         import java.io.IOException;
+         import java.io.InputStreamReader;
+         import java.io.OutputStreamWriter;
+         import java.io.StringWriter;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+         import android.app.Activity;
+         import android.os.Bundle;
+         import android.os.Environment;
+         import android.util.Log;
+         import android.view.View;
+         import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+         public class MainActivity extends AppCompatActivity {
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void onClick(View view) {
-        String Words = new String("Here I am");
-
-        try {
-            String fname = "input1";
-            File filename = new File(getApplicationContext().getFilesDir(),fname);
-            /*FileOutputStream fout = openFileOutput(filename.toString(), 1);
-            OutputStreamWriter osw = new OutputStreamWriter(fout);
-// записываем строку в файл
-            osw.write(Words);
-            osw.flush();
-            osw.close();*/
-            FileWriter f = new FileWriter(filename, true);
-            StringWriter fout = new StringWriter();
-            fout.write(Words);
-            f.write(fout.toString());
-            fout.close();
-            f.close();
-
-        } catch (Exception e) {
-            //вывод сообщения об ошибке
-            TextView Error=(TextView) findViewById(R.id.textView);
-            Error.setText("we are in the shit11111");
-        }
-
-// записываем строку в файл
-
-
-        try{
-
-            String fname = "input1";
-            File filename = new File(getApplicationContext().getFilesDir(),fname);
-            int c=0, num=0;
-            float X, Y;
-            double [][] coords = new double[1000][3];
-            FileReader g = new FileReader(filename);
-            BufferedReader br = new BufferedReader(g);
-            String s="";
-            s=br.readLine();
-            for(String si:s.split(" ")){
-                if(c==0){
-                    X= Float.parseFloat(si);
-                }
-                else if(c==1){
-                    Y= Float.parseFloat(si);
-                }
-                else{
-                    coords[c-2][num]=Float.parseFloat(si);
-                    num++;
-                    if(num==3){num=0;}
-                }
-                c++;
+             @Override
+     protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
             }
+            public void onClick(View view) {
+                String Words = new String("1.4 6.98 8.5693 9.0 5.3");
 
-            TextView Result=(TextView) findViewById(R.id.textView);
-            Result.setText("Everuthing alright");
+                try {
+                        String fname = "input1";
+                        File filename = new File(getApplicationContext().getFilesDir(),fname);
+                        FileWriter f = new FileWriter(filename, false);
+                        StringWriter fout = new StringWriter();
+                        fout.write(Words);
+                        f.write(fout.toString());
+                        fout.close();
+                        f.close();
+                }
+                catch (Exception e) {
+                            TextView Error=(TextView) findViewById(R.id.textView);
+                            Error.setText("we are in the shit11111");
+                }
 
-        } catch (Exception e) {
-            //вывод сообщения об ошибке
-            TextView Error=(TextView) findViewById(R.id.textView);
-            Error.setText("we are in the shit22222");
-        }
 
+                try{
+                        String fname = "input1", s="";
+                        int c=0, num=0;
+                        File filename = new File(getApplicationContext().getFilesDir(),fname);
+                        float [][] ResCoords = new float[1000][3];
+                        FileReader g = new FileReader(filename);
+                        BufferedReader br = new BufferedReader(g);
+                        s=br.readLine();
+                        for(String si:s.split(" ")){
+                            ResCoords[c][num]=Float.parseFloat(si);
+                            num++;
+                            if(num==3){num=0;c++;}
 
-    }
-}
+                        }
+                        TextView Result1=(TextView) findViewById(R.id.textView);
+                        Result1.setText(s.toString());
+                }
+                catch (Exception e) {
+                                TextView Error=(TextView) findViewById(R.id.textView);
+                        Error.setText("we are in the shit22222");
+                }
 
-
+            }
+ }
